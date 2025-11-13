1f2618e520f9b0563c39ca734c899c9d627932eb
Tarundip Singh <tarundip9990@gmail.com>
2025-11-13 06:14:46 +0530

    server-client origin integration

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ await connectDB();
 await connectCloudinary();
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://snapit-7983bd6d1-tarundip-singhs-projects.vercel.app']
 
 app.post('/stripe', express.raw({ type: "application/json" }), stripeWebhooks)
 
